7dd9619e7873b7a9bcdba42298ff26d2091cb0cf
Aditya <adityaislegend@outlook.com>
2024-05-30 23:28:35 +0530

    native

---
+++ RESOURCE/React Native/02 React Core Concepts/hello/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default App = () => {
+  return (
+    <View style={[{
+      backgroundColor:"red"
+    }]}><Text>h</Text></View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
